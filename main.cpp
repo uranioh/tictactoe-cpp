c77b3d3101aca4f85713d0ee2548df59d5db57e8
Alessandro <melis.alessandro05@gmail.com>
2022-06-07 17:45:42 +0200

    Final version code // handed to teacher
    this shit ain't object oriented dude

--- main.cpp
+++ main.cpp
@@ -1,86 +1,115 @@
 #include <iostream>
 using namespace std;
 
-// cls = windows
-// clear = unix
-
+// definisco la dimensione del campo di gioco
 const int fieldSize = 3;
 char gameMatrix[fieldSize][fieldSize];
 
 
+// reset del campo di gioco
 void resetField() {
-    for (int i=0; i<fieldSize; i++) {
-        for (int j=0; j<fieldSize; j++) {
+    for (int i=0; i < fieldSize; i++) {
+        for (int j=0; j < fieldSize; j++) {
             gameMatrix[i][j] = ' ';
         }
     }
 }
 
 
+// mostro il campo di gioco a schermo
 void showField() {
-    cout << endl;
     cout << "   0   1   2" << endl;
-    for (int i=0; i<fieldSize; i++) {
+    for (int i=0; i < fieldSize; i++) {
         cout << i << " ";
-        for (int j=0; j<fieldSize; j++) {
+        for (int j=0; j < fieldSize; j++) {
             cout << "[" << gameMatrix[i][j] << "] ";
         }
         cout << endl;
     }
 }
 
+
+// clear della console
 void clearScreen() {
-    // cls = windows
-    // clear = unix
+    // windows = "cls"
+    // unix-like = "clear"
     system("clear");
 }
 
+// inserimento del carattere nelle caselle
 bool insertChar(char player, int row, int col) {
-    // check empty section
-    if (row >= 0 && row < 3 && col >= 0 && col < 3) {
+    bool returnStatus = false;
+    // parametri invalidi:
+
+    // righe e colonne valide: 0, 1, 2
+    if (row >= 0 && row < fieldSize && col >= 0 && col < fieldSize) {
+        // controllo se dove l'utente vuole posizionare una x/o è libero
         if (gameMatrix[row][col] == ' ') {
-                if (player == 'x' || player == 'o') {
-                    gameMatrix[row][col] = player;
-                    return true;
-                }
-                else {
-                    cout << "Carattere non valido.";
-                    return false;
-                }
+            gameMatrix[row][col] = player;
+            returnStatus = true;
         }
         else {
-                cout << "La casella non e' vuota.";
-                return false;
+            cout << "La casella non e' vuota." << endl;
         }
     }
     else {
-        cout << "Coordinate non valide.";
-        return false;
+        cout << "Coordinate non valide." << endl;
     }
+
+    return returnStatus;
 }
 
 
+// controllo dei vincitori / stato di gioco generale
 char checkWinner() {
-    char winner;
-    for (int i=0; i<fieldSize; i++) {
+    // n = none
+    // d = draw
+    // x = vince il giocatore x
+    // o = vince il giocatore o
+    char winner = 'n';
+    for (int i=0; i < fieldSize; i++) {
+
+        // controllo se le righe sono uguali
         if (gameMatrix[i][0] == gameMatrix[i][1] && gameMatrix[i][0] == gameMatrix[i][2]) {
             if (gameMatrix[i][0] != ' ') {
                 winner = gameMatrix[i][0];
-                break;
             }
-            else winner = 'n';
         }
+
+        // controllo se le colonne sono uguali
         else if (gameMatrix[0][i] == gameMatrix[1][i] && gameMatrix[0][i] == gameMatrix[2][i]) {
             if (gameMatrix[0][i] != ' ') {
                 winner = gameMatrix[0][i];
-                break;
             }
-            else winner = 'n';
-        }
-        else {
-            winner = 'n';
         }
     }
+
+    if (gameMatrix[1][1] != ' ') {
+        // diagonale da sinistra a destra
+        if (gameMatrix[0][0] == gameMatrix[1][1] && gameMatrix[0][0] == gameMatrix[2][2]) {
+            winner = gameMatrix[1][1];
+        }
+
+        // diagonale da destra a sinistra
+        if (gameMatrix[0][2] == gameMatrix[1][1] && gameMatrix[0][2] == gameMatrix[2][0]) {
+            winner = gameMatrix[1][1];
+        }
+    }
+
+    // controllo situazione di pareggio
+    int placedCount = 0;
+    for (int i=0; i < fieldSize; i++) {
+        for (int j=0; j < fieldSize; j++) {
+            if (gameMatrix[i][j] != ' ') {
+                placedCount++;
+            }
+        }
+    }
+
+    if (placedCount == 9) {
+        winner = 'd';
+    }
+
     return winner;
 }
 
@@ -88,7 +117,7 @@ char checkWinner() {
 int main() {
     int gameStatus = 0;
     char currentPlayer = 'x';
-    int row; int col;
+    int row, col;
 
     clearScreen();
     resetField();
@@ -104,8 +133,9 @@ int main() {
         cout << "Inserisci la colonna: ";
         cin >> col;
 
-
-
+        clearScreen();
+        // verifico se il giocatore x compie una mossa valida
+        // se sì passo al giocatore o, altrimenti faccio ripetere la mossa e viceversa
         if (insertChar(currentPlayer, row, col)) {
             if (currentPlayer == 'x') {
                 currentPlayer = 'o';
@@ -115,13 +145,23 @@ int main() {
             }
         }
 
+        // condizione di vincita:
+        // se il char di return della funzione checkWinner è diverso da N significa che
+        // sarà x oppure o, di conseguenza qualcuno ha vinto.
         if (checkWinner() != 'n') {
-            cout << "Il giocatore [" << checkWinner() << "] vince!" << endl;
+            clearScreen();
+            showField();
+
+            if (checkWinner() != 'd') {
+                cout << "Il giocatore [" << checkWinner() << "] vince!" << endl;
+            }
+            else {
+                cout << "La partita si conclude in pareggio." << endl;
+            }
+
             gameStatus = 1;
         }
-        else {
-            clearScreen();
-        }
     }
+    
     return 0;
 }
